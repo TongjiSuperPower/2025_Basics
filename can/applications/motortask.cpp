9dfaf8eae484f556c2b2d22c41137ab6ad53b086
lyfeng001 <774033660@qq.com>
2024-06-20 09:55:20 +0800

    refine

--- can/applications/motortask.cpp
+++ can/applications/motortask.cpp
@@ -1,91 +1,71 @@
 #include "motortask.h"
-#include "main.h"
-#include "cmsis_os.h"
 
-extern "C"
-{
+#include "cmsis_os.h"
+#include "main.h"
+
+extern "C" {
 
 extern CAN_HandleTypeDef hcan1;
-static CAN_TxHeaderTypeDef  motor_tx_message;
-static uint8_t              motor_can_send_data[8];
+static CAN_TxHeaderTypeDef motor_tx_message;
+static uint8_t motor_can_send_data[8];
 
+Motor::Motor() {}
 
+Motor::~Motor() {}
 
-Motor::Motor()
-{}
-
-Motor::~Motor()
-{}
-
-motor_measure_t* Motor::get_motor_measure()
-{
-    return &motor_meature;
-}
+motor_measure_t * Motor::get_motor_measure() { return &motor_meature; }
 
 void Motor::motor_cmd(int16_t motor)
 {
-    uint32_t send_mail_box;
-    motor_tx_message.StdId = 0x1ff;
-    motor_tx_message.IDE = CAN_ID_STD;
-    motor_tx_message.RTR = CAN_RTR_DATA;
-    motor_tx_message.DLC = 0x08;
-    motor_can_send_data[0] = (motor >> 8);
-    motor_can_send_data[1] = motor;
-    motor_can_send_data[2] = 0;
-    motor_can_send_data[3] = 0;
-    motor_can_send_data[4] = 0;
-    motor_can_send_data[5] = 0;
-    motor_can_send_data[6] = 0;
-    motor_can_send_data[7] = 0;
-    HAL_CAN_AddTxMessage(&hcan1, &motor_tx_message, motor_can_send_data, &send_mail_box);
+  uint32_t send_mail_box;
+  motor_tx_message.StdId = 0x200;
+  motor_tx_message.IDE = CAN_ID_STD;
+  motor_tx_message.RTR = CAN_RTR_DATA;
+  motor_tx_message.DLC = 0x08;
+  motor_can_send_data[0] = (motor >> 8);
+  motor_can_send_data[1] = motor;
+  motor_can_send_data[2] = 0;
+  motor_can_send_data[3] = 0;
+  motor_can_send_data[4] = 0;
+  motor_can_send_data[5] = 0;
+  motor_can_send_data[6] = 0;
+  motor_can_send_data[7] = 0;
+  HAL_CAN_AddTxMessage(&hcan1, &motor_tx_message, motor_can_send_data, &send_mail_box);
 }
 
-void Motor::decode_motor_measure(motor_measure_t *ptr, uint8_t data[8])                                    
-{                                                                   
-    (ptr)->last_ecd = (ptr)->ecd;                                   
-    (ptr)->ecd = (uint8_t)((data)[0] << 8 | (data)[1]);            
-    (ptr)->speed_rpm = (uint8_t)((data)[2] << 8 | (data)[3]);      
-    (ptr)->given_current = (uint8_t)((data)[4] << 8 | (data)[5]);  
-    (ptr)->temperate = (data)[6];                                   
+void Motor::decode_motor_measure(motor_measure_t * ptr, uint8_t data[8])
+{
+  (ptr)->last_ecd = (ptr)->ecd;
+  (ptr)->ecd = (uint16_t)((data)[0] << 8 | (data)[1]);
+  (ptr)->speed_rpm = (int16_t)((data)[2] << 8 | (data)[3]);
+  (ptr)->given_current = (int16_t)((data)[4] << 8 | (data)[5]);
+  (ptr)->temperate = (uint16_t)(data)[6];
 }
-
 
 // -------------------------------------------------------------------------------------- //
 // -------------------------------------------------------------------------------------- //
-
 
 Motor motor_6020;
 void motor_task()
 {
-    while(1)
-    {
-        motor_6020.motor_cmd(2000);
-        vTaskDelay(1);
-    }
+  while (1) {
+    motor_6020.motor_cmd(200);
+    vTaskDelay(1);
+  }
 }
 
-
-
 // -------------------------------------------------------------------------------------- //
 // -------------------------------------------------------------------------------------- //
 
-void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef * hcan)
 {
-    CAN_RxHeaderTypeDef rx_header;
-    uint8_t rx_data[8];
+  CAN_RxHeaderTypeDef rx_header;
+  uint8_t rx_data[8];
 
-    HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rx_header, rx_data);
-    
-    if(rx_header.StdId == 0x205)
-    {
-        motor_6020.decode_motor_measure(motor_6020.get_motor_measure(), rx_data);
-    }
+  HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rx_header, rx_data);
+
+  if (rx_header.StdId == 0x201) {
+    motor_6020.decode_motor_measure(motor_6020.get_motor_measure(), rx_data);
+  }
 }
-
-
-
-
-
 }
-
-
--- can/applications/motortask.h
+++ can/applications/motortask.h
@@ -1,41 +1,34 @@
 #include <cstdint>
+
 #include "main.h"
 #ifndef MOTOR_TASK_H
 #define MOTOR_TASK_H
 
-extern "C"
-{
+extern "C" {
 
 typedef struct
 {
-    uint16_t ecd;
-    int16_t speed_rpm;
-    int16_t given_current;
-    uint8_t temperate;
-    int16_t last_ecd;
-	
-} motor_measure_t;
+  uint16_t ecd;
+  int16_t speed_rpm;
+  int16_t given_current;
+  uint16_t temperate;
+  uint16_t last_ecd;
 
+} motor_measure_t;
 
 class Motor
 {
 public:
-    Motor();
-    ~Motor();
-    void motor_cmd(int16_t motor);
-    motor_measure_t *get_motor_measure();
-    void decode_motor_measure(motor_measure_t *ptr, uint8_t data[8]);
+  Motor();
+  ~Motor();
+  void motor_cmd(int16_t motor);
+  motor_measure_t * get_motor_measure();
+  void decode_motor_measure(motor_measure_t * ptr, uint8_t data[8]);
 
 private:
-
-    motor_measure_t motor_meature;
-
+  motor_measure_t motor_meature;
 };
 
-
 void motor_task();
-
 }
-#endif // MOTOR_TASK_H
-
-
+#endif  // MOTOR_TASK_H
