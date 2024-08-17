fcce5af33f59b3f8f54acdb9617e0a052e67ed15
chmwt <1102453984@qq.com>
2024-08-17 18:49:21 +0800

    修改带DMA的uart

---
+++ chapter7 USART/Code/UART_DMA/applications/uart_DMA.cpp
@@ -0,0 +1,31 @@
+#include "cmsis_os.h"
+#include "usart.h"
+
+#pragma pack(1)
+struct PlotFrame
+{
+  uint8_t start[2] = {0xAA, 0xBB};
+  uint8_t size;
+  float data;
+};
+#pragma pack()
+
+PlotFrame plot_frame;
+
+uint8_t i = 0;
+extern "C" void uart_send_task()
+{
+  while (1) {
+    plot_frame.size = 4 * 1;
+    plot_frame.data = i;
+
+    HAL_UART_Transmit_DMA(
+      &huart1, (uint8_t *)&plot_frame,
+      sizeof(plot_frame.start) + sizeof(plot_frame.size) + plot_frame.size);
+
+    i = i + 1;
+    if (i >= 5) i = 0;
+
+    vTaskDelay(10);
+  }
+}
